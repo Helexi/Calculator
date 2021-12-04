cd779c73b30bfbf965f03d9d967d28a67477b03c
Helexi <ruslangubanov88@gmail.com>
2021-12-04 17:12:46 +0200

    2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const calcScreen = document.querySelector('.calc-screen');
 const ac = document.querySelector('.ac');
-const buttons  = document.querySelector('.buttons');
+const buttons = document.querySelector('.buttons');
 
 let a = '' // первое число
 let b = '' // второе число
@@ -8,19 +8,87 @@ let sing = '' // знак вычисления
 let finich = false;
 
 let namber = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
-let action = ['-', '+', 'x', '/']
+let action = ['-', '+', 'x', '/'];
 
-function clearAll () {
+function clearAll() {
     a = '' // первое число
     b = '' // второе число
     sing = '' // знак вычисления 
     finich = false;
     calcScreen.textContent = 0;
-    console.log(1);
 }
 
-function Nabor () {
-    console.dir(buttons);
+
+function Nabor(event) {
+    //нажата не кропка
+    if (!event.target.classList.contains('btn')) {
+        return;
+    }
+    // нажата кнопка ac
+    if (event.target.classList.contains('ac')) {
+        calcScreen.textContent = "";
+        return;
+    }
+    // получаю нажатую кнопку
+    const key = event.target.textContent;
+
+    //если нажата клавиша 0-9 или .
+
+    if (namber.includes(key)) {
+        if (b === '' && sing === "") {
+            a += key;
+            console.log(a, b, sing);
+            calcScreen.textContent = a;
+        } else if (a !== "" && b !== "" && finich) {
+            b = key;
+            finich = false;
+            calcScreen.textContent = b;
+        } else {
+            b += key;
+            calcScreen.textContent = b;
+        }
+        return;
+    }
+
+    //если нажата клавиша + - / *
+
+    if (action.includes(key)) {
+        sing = key;
+        calcScreen.textContent = sing;
+        return;
+    };
+
+    //если нажата клавиша =
+
+    if (key === '=') {
+        if (b === "") {
+            b = a;
+        }
+        switch (sing) {
+            case "+":
+                a = (+a) + (+b);
+                break;
+            case '-':
+                a = a - b;
+                break;
+            case 'x':
+                a = a * b;
+                break;
+            case '/':
+                if (b === '0') {
+                    calcScreen.textContent = 'Ошибка';
+                    a = '';
+                    b = '';
+                    sing = '';
+                    return;
+                }
+                a = a / b;
+                break;
+        }
+    }
+    finich = true;
+    calcScreen.textContent = a;
+    console.log(a, b, sing);
 }
 
 ac.addEventListener('click', clearAll);
